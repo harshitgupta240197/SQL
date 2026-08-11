57d4809f94053406bea69f55e9ae7baca753a064
Harshit Gupta <biogenius6@gmail.com>
2026-08-11 16:22:38 +0530

    leetcode - 176

---
+++ MySQL/LeetCode/176.sql
@@ -0,0 +1,9 @@
+-- 176. Second Highest Salary
+SELECT
+    MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (
+    SELECT
+        MAX(salary) AS HighestSalary
+    FROM Employee
+)

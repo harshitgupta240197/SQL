65c46ba4da552ae2c938d11993dcad6fd1de9656
Harshit Gupta <biogenius6@gmail.com>
2026-08-12 20:14:14 +0530

    leetcode - 1393

---
+++ MySQL/LeetCode/1393.sql
@@ -0,0 +1,7 @@
+-- 1393. Capital Gain/Loss
+SELECT 
+    stock_name,
+    (SUM(CASE WHEN operation = 'Sell' THEN price END) - 
+    SUM(CASE WHEN operation = 'Buy' THEN price END)) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name

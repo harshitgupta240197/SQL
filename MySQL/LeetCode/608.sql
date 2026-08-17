0be656f533092a225ff4d9b65a9268a81e1b393e
Harshit Gupta <biogenius6@gmail.com>
2026-08-17 17:44:19 +0530

    leetcode - 608

---
+++ MySQL/LeetCode/608.sql
@@ -0,0 +1,9 @@
+--  608. Tree Node
+SELECT
+    id,
+    CASE
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (SELECT p_id FROM Tree) THEN 'Inner'
+        ELSE 'Leaf'
+    END as type
+FROM Tree
